bd9685f915a4fec25f7b6a3b40181bbd5c94f2ac
atria23 <danu03anggoro@gmail.com>
2024-08-06 10:20:50 +0700

    add weather proxy

--- api/proxy.js
+++ api/proxy.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
     }
   
     // Gantilah 'https://api.example.com' dengan URL API sebenarnya
-    fetch('https://api.openweathermap.org/data/2.5' + req.url)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=3b23211a2f77b785278502f01fffba5f' + req.url)
       .then(response => response.json())
       .then(data => res.status(200).json(data))
       .catch(error => res.status(500).json({ error: 'Something went wrong' }));

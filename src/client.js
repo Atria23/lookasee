322ad218b4bf153306a17abc3ccff9172277850f
Atria23 <134146550+Atria23@users.noreply.github.com>
2025-06-13 20:21:50 +0700

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tnrkvhyahgvlvepjccvq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucmt2aHlhaGd2bHZlcGpjY3ZxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTgxMDcwNCwiZXhwIjoyMDY1Mzg2NzA0fQ.eJmU-RVlAkpDfVg6spAIhym-uG1tMcHCkNRtYm50nlU'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucmt2aHlhaGd2bHZlcGpjY3ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MTA3MDQsImV4cCI6MjA2NTM4NjcwNH0.iKM_YYDpgNqnUMNrhGa56BkGSs23LwyPe1w1AS9FAIQ'
 export const supabase = createClient(supabaseUrl, supabaseKey)
